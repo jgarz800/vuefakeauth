2a7fdc78d60cb1265cf9b9ba85a1640fda1a6cd5
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-03-06 00:52:47 -0600

    secret page added

---
+++ src/pages/secret.vue
@@ -0,0 +1,6 @@
+<template>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam delectus quo cupiditate inventore, unde dolores, architecto error aliquam quisquam laudantium perspiciatis ab doloribus facilis, corrupti praesentium numquam ratione rem accusantium.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure autem delectus quos provident reprehenderit, aperiam fuga libero doloribus est repudiandae saepe nulla in quaerat dicta debitis eius natus odio?
+    </p>
+</template>
